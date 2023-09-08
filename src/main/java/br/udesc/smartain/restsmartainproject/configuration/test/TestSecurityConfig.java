22f312c71f17c68ba1b17656f89d75a29b9f25cc
Matheus Buschermoehle <matheusbuschermoehle@gmail.com>
2023-09-07 23:08:43 -0300

    feat(Security)

--- src/main/java/br/udesc/smartain/restsmartainproject/configuration/test/TestSecurityConfig.java
+++ src/main/java/br/udesc/smartain/restsmartainproject/configuration/test/TestSecurityConfig.java
@@ -3,9 +3,12 @@ package br.udesc.smartain.restsmartainproject.configuration.test;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @Profile("test")
@@ -16,11 +19,20 @@ public class TestSecurityConfig {
 
         UserDetails matheus = User.builder()
                 .username("matheus")
-                .password("12345")
+                .password("{noop}12345")
                 .roles("ADMIN")
                 .build();
 
         return new InMemoryUserDetailsManager(matheus);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.cors((cors) -> cors.disable());
+        http.csrf((crsf) -> crsf.disable());
+
+        return http.build();
     }
 
 }

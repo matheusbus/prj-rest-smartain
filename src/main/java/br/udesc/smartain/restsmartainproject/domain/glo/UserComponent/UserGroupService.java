0cfa8008e4adbac55a57f22bd96a57efeea764b7
EduardoSangaletti <eduardosangaletti@gmail.com>
2023-11-26 16:57:10 -0300

    user
    a

--- src/main/java/br/udesc/smartain/restsmartainproject/domain/glo/UserComponent/UserGroupService.java
+++ src/main/java/br/udesc/smartain/restsmartainproject/domain/glo/UserComponent/UserGroupService.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import br.udesc.smartain.restsmartainproject.domain.mhu.SectorComponent.Sector;
+
 import java.util.List;
 import java.util.Optional;
 
@@ -16,6 +18,11 @@ public class UserGroupService {
     @Transactional(readOnly = true)
     public Optional<List<UserGroup>> findAllUserGroups() {
         return userGroupRepository.findAllGroups();
+    }
+
+    @Transactional(readOnly = true)
+    public Optional<UserGroup> findById(Long id) {
+        return userGroupRepository.findById(id);
     }
 
     @Transactional
